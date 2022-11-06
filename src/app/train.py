de01ad8e668c5bc3026bc216bcfb8a9917e0018c
s16892 <s16892@pjwstk.edu.pl>
2022-11-06 18:34:10 +0100

    train.py initial implementation

--- src/app/train.py
+++ src/app/train.py
@@ -1,15 +1,20 @@
-import pandas as pd
-import numpy as np
-
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+import warnings
+warnings.filterwarnings('ignore')
 
-from sklearn.preprocessing import MinMaxScaler
 
-def prepare_data_for_modeling(df):
+def prepare_data_for_modeling(dataset):
     '''Prepare data for modeling
     
     Inputs:
@@ -21,34 +26,26 @@ def prepare_data_for_modeling(df):
         y: a pandas series with customer labels, encoded as integers (0 = low revenue, 1 = high revenue)
 
     '''
-    # Suppress "a copy of slice from a DataFrame is being made" warning
-    pd.options.mode.chained_assignment = None
+
+    #Encoding categorical value
+    le = LabelEncoder()
+
+    dataset['Education']=le.fit_transform(dataset['Education'])
+    dataset['Gender']=le.fit_transform(dataset['Gender'])
+    dataset['EverBenched']=le.fit_transform(dataset['EverBenched'])
+
+    dataset = pd.get_dummies(dataset, drop_first=False)
+
+    #Dataset Reordering
+    leave_or_not = dataset['LeaveOrNot']
+    dataset = dataset.drop('LeaveOrNot', axis=1)
+    dataset.insert(loc=len(dataset.columns), column='LeaveOrNot', value=leave_or_not)
     
-    # prepare dataset for classification
-    features = df.columns[:-1]
-    X = df[features]
-    y = df['high_revenue']
+    dataset = dataset.drop(['JoiningYear', 'PaymentTier'], axis=1)
 
-    # identify numeric features in X
-    numeric_features = X.select_dtypes(include=[np.number]).columns
-    # identify categorical features in X
-    categorical_features = X.select_dtypes(exclude=[np.number]).columns
+    return dataset
 
-    # Normalize numeric features with MinMaxScaler
-    scaler = MinMaxScaler()
-    X[numeric_features] = scaler.fit_transform(X[numeric_features])
-
-    # One-hot encode categorical features
-    X = pd.get_dummies(X, columns=categorical_features)
-
-    # transform bool in y to 0/1
-    y = y.astype(int)
-    
-    #create a dataframe with the features and the labels
-    data_prepared = pd.concat([X, y], axis=1)
-    return data_prepared
-
-def train(df):    
+def train(dataset):    
     '''Train a model predicting high-revenue customers from features and labels
     
     Inputs:
@@ -58,37 +55,45 @@ def train(df):
     model: a trained model
     
     '''
-    
-    # Suppress "a copy of slice from a DataFrame is being made" warning
-    pd.options.mode.chained_assignment = None
-    
-    features = df.columns[:-1]
-    X = df[features]
-    y = df['high_revenue']
-    
-    # split dataset into train and test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    #transforming dataset to array
+    x = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
 
+    #separating training set and test set
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=27)
 
-    # train the model
-    #model = DecisionTreeClassifier()
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
+    #scaling the training set
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
 
-    # test the model
-    y_pred = model.predict(X_test)
+    #Model Building and Training with the Training Set
+    classifier = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=27)
+    classifier.fit(x_train, y_train)
 
-    # evaluate the model
+    #Predicting the Test Set Result
+    y_pred = classifier.predict(x_test)
+    result_np = np.concatenate((y_pred.reshape(len(y_pred), 1), (y_test.reshape(len(y_test), 1))), 1)
+    result = pd.DataFrame(result_np, columns=['Prediction', 'Real_Value'])
+    print(result)
+
+    #Accuracy report
+    print(classification_report(y_test, y_pred))
+
+    #Accuracy Report with K-Fold Cross Validation
+    val_score = cross_val_score(estimator=classifier, X = x_train, y=y_train, cv=10)
+    print("Accuracy: {:.2f} %".format(val_score.mean()*100))
+    print("Std. Dev: {:.2f} %".format(val_score.std()*100))
+
     
-    #print(accuracy_score(y_test, y_pred))
-    print(metrics.classification_report(y_test, y_pred))
-    
-    return model
+    return classifier
 
 # Read customers_labeled dataset
 
-df = pd.read_csv('data/03_prepared/customers_labeled.csv')
+df = pd.read_csv('src/app/test.csv') #data/03_prepared/data.csv
 
 data_prepared = prepare_data_for_modeling(df)
 # Train the model
 model = train(data_prepared)
+
+
