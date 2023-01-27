efd809adfde3c98208210e7858bac41a3ea8ed8c
suzejka <zuzanna.julia.stasiak@gmail.com>
2023-01-27 13:48:20 +0100

    Added second router

--- src/solitune/routers/data_processing/data_processing.py
+++ src/solitune/routers/data_processing/data_processing.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from kedro.pipeline import Pipeline
-from solitune.dependencies.dependencies import encode_categorical_pipeline
+from solitune.dependencies.dependencies import encode_categorical_pipeline, reorder_dataset_pipeline
 from kedro.io import DataCatalog
 from kedro.runner import SequentialRunner
 from typing import Any
@@ -21,3 +21,11 @@ def encode_categorical_values(
     runner = SequentialRunner()
     catalog = DataCatalog(feed_dict={"employee": employee})
     return runner.run(pipeline=pipeline, catalog=catalog)["employee_encoded"]
+
+
+@router.get("/reorder_dataset")
+def reorder_dataset(
+    pipeline: Pipeline = Depends(reorder_dataset_pipeline)) -> dict[str, Any]:
+    runner = SequentialRunner()
+    catalog = DataCatalog()
+    return runner.run(pipeline=pipeline, catalog=catalog)
